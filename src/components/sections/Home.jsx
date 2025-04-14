896dfba55610dfc121784f6a24235974e6ba0c7b
Vishnu P S <vishnu13three@gamil.com>
2025-04-14 19:24:44 +0530

    about section  need to add proper education and experiences

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -3,7 +3,7 @@ export const Home = () => {
     return (
         <section id="home" className="min-h-screen flex items-center justify-center relative">
             <div className="text-center z-10 px-4">
-                <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent leading-right">Hi, I am Ghost</h1>
+                <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-right">Hi, I am Ghost</h1>
 
                 <p className="text-grey-400 text-lg mb-8 max-2-lg mx-auto">
                     Aspiring full-Stack developer
